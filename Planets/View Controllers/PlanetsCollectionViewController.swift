5aa596cb8c536b29f941dfa8afc027a4110d4365
seanacres <6416690+seanacres@users.noreply.github.com>
2019-06-13 13:07:32 -0400

    Finished setting up collection view to display planets

--- Planets/View Controllers/PlanetsCollectionViewController.swift
+++ Planets/View Controllers/PlanetsCollectionViewController.swift
@@ -12,17 +12,25 @@ private let reuseIdentifier = "PlanetCell"
 
 class PlanetsCollectionViewController: UICollectionViewController {
 
+    let planetController = PlanetController()
     // MARK: UICollectionViewDataSource
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
+    }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 1
+        return planetController.planets.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? PlanetCollectionViewCell else { return UICollectionViewCell() }
+        
+        let planet = planetController.planets[indexPath.item]
+        cell.imageView.image = planet.image
+        cell.nameLabel.text = planet.name
     
         return cell
     }
---
+++ Planets/Views/PlanetCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Sean Acres on 6/13/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
